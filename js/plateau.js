fa09f927e8749100202332c0dbd62454959e6b8f
Mohandor <Emilio.Cadario@gmail.Com>
2017-07-12 21:20:28 +0300

    Début du projet, premier test sur la génération du tableau

---
+++ js/plateau.js
@@ -0,0 +1,36 @@
+/*var plateau = {
+	nbLignes : 12,
+	nbColones : 12,
+
+	generationPlateauVide: function() {
+
+		for (var i = 1; i <= this.nbLignes; i++) {
+				
+			$('<div/>').addClass('row').attr('id', 'row'+i).appendTo($("#boardGame"));
+
+				for (var j = 1; j <= this.nbColones; j++) {
+					$('<div/>').addClass('col-xs-1 colBoardGame col'+j).appendTo($('#row'+i));
+					console.log(12*(i-1)+j);
+				}
+		}
+	}
+}*/
+
+
+var plateau = {
+	nbLignes : 12,
+	nbColones : 12,
+
+	generationPlateauVide: function() {
+
+		for (var i = 1; i <= this.nbLignes; i++) {
+				
+			$('<div/>').addClass('row').attr('id', 'row'+i).appendTo($("#boardGame"));
+
+				for (var j = 1; j <= 12; j++) {
+					$('<div/>').addClass('col-xs-1 colBoardGame').attr('id','case'+(this.nbLignes*(i-1)+j)).text(this.nbLignes*(i-1)+j).appendTo($('#row'+i));
+					console.log(12*(i-1)+j);
+				}
+		}
+	}
+}	
