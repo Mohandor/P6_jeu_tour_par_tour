0d231a116593e203ddf628b781653219fe2174fb
Mohandor <Emilio.Cadario@gmail.Com>
2017-08-01 13:45:59 +0200

    Modification de la fonction verification pour qu'elle s'arrête sur les cases weapon et soit plus courte

--- js/mouvement.js
+++ js/mouvement.js
@@ -1,166 +1,125 @@
 function verification(position) {
+	var positionVerification = arguments[0];
+
+	function verificationDroite(positionVerification){
+		for (var m=1;m<4;m++){
+			if ($('#'+(positionVerification+m)).hasClass('weapon')){
+				$('#'+(positionVerification+m)).addClass('movementPossible');
+				break;
+			}else if ($('#'+(positionVerification+m)).hasClass('empty')) {
+				$('#'+(positionVerification+m)).addClass('movementPossible');
+			} else {break;}
+		}
+	}
+	function verificationDroiteModulo(positionVerification){
+		for (var m=1;m<4;m++){
+			if (((positionVerification+m)%plateau.nbLignes===0) & ($('#'+(positionVerification+m)).hasClass('empty') || $('#'+(positionVerification+m)).hasClass('weapon'))) {
+				$('#'+(positionVerification+m)).addClass('movementPossible');
+				break;
+			} else if ($('#'+(positionVerification+m)).hasClass('weapon')){
+				$('#'+(positionVerification+m)).addClass('movementPossible');
+				break;
+			} else if ($('#'+(positionVerification+m)).hasClass('empty')) {
+				$('#'+(positionVerification+m)).addClass('movementPossible');
+			} else {break;}
+		}
+	}
+	function verificationGauche(positionVerification){
+		for (var m=1;m<4;m++){
+			if ($('#'+(positionVerification-m)).hasClass('weapon')){
+				$('#'+(positionVerification-m)).addClass('movementPossible');
+				break;
+			}else if ($('#'+(positionVerification-m)).hasClass('empty')){
+				$('#'+(positionVerification-m)).addClass('movementPossible');
+			} else {break;}
+		}
+	}
+	function verificationGaucheModulo(positionVerification){
+		for (var m=1;m<4;m++){positionVerification
+			if (((positionVerification-m)%plateau.nbLignes===1) & ($('#'+(positionVerification-m)).hasClass('empty') || $('#'+(positionVerification-m)).hasClass('weapon'))) {
+				$('#'+(positionVerification-m)).addClass('movementPossible');
+				break;
+			} else if ($('#'+(positionVerification-m)).hasClass('weapon')){
+				$('#'+(positionVerification-m)).addClass('movementPossible');
+				break;
+			}else if ($('#'+(position-m)).hasClass('empty')) {
+				$('#'+(positionVerification-m)).addClass('movementPossible');
+			} else {break;}
+		}
+	}
+	function verificationBas(positionVerification){
+		for (var m=1; m<4; m++){
+			if ($('#'+(positionVerification+plateau.nbLignes*m)).hasClass('weapon')){
+				$('#'+(positionVerification+plateau.nbLignes*m)).addClass('movementPossible');
+				break;
+			}else if ($('#'+(positionVerification+plateau.nbLignes*m)).hasClass('empty')) {
+				$('#'+(positionVerification+plateau.nbLignes*m)).addClass('movementPossible');
+			} else {break;}
+		}
+	}
+	function verificationHaut(positionVerification){
+		for (var m=1;m<4;m++){
+			if ($('#'+(positionVerification-plateau.nbColones*m)).hasClass('weapon')){
+				$('#'+(positionVerification-plateau.nbColones*m)).addClass('movementPossible');
+				break;
+			}else if ($('#'+(positionVerification-plateau.nbColones*m)).hasClass('empty')) {
+				$('#'+(positionVerification-plateau.nbColones*m)).addClass('movementPossible');
+			} else {break;}	
+		}
+	}
+
 
 	// Coin haut gauche
 	if ( $('#'+position).hasClass('col1') & $('#'+position).is('#row1') ) {
+		verificationDroite(positionVerification);
+		verificationBas(positionVerification);
 
-		for (var m=1;m<4;m++){
-			if ($('#'+(position+m)).hasClass('empty') || $('#'+(position+m)).hasClass('weapon') ) {
-				$('#'+(position+m)).addClass('movementPossible');
-			} else {break;}
-		}
-		for (var m=1; m<4; m++){
-			if ($('#'+(position+plateau.nbLignes*m)).hasClass('empty') || $('#'+(position+m)).hasClass('weapon')) {
-				$('#'+(position+plateau.nbLignes*m)).addClass('movementPossible');
-			} else {break;}
-		}
 
 	// Coin haut droite
 	} else if ( $('#'+position).hasClass('col'+plateau.nbColones) & $('#'+position).is('#row1')) {
-		for (var m=1;m<4;m++){
-			if ($('#'+(position-m)).hasClass('empty') || $('#'+(position-m)).hasClass('weapon') ){
-				$('#'+(position-m)).addClass('movementPossible');
-			} else {break;}
-		}
-		for (var m=1;m<4;m++){
-			if ($('#'+(position+plateau.nbColones*m)).hasClass('empty') || $('#'+(position+plateau.nbColones*m)).hasClass('weapon')) {
-				$('#'+(position+plateau.nbColones*m)).addClass('movementPossible');
-			} else {break;}			
-		}
+		verificationGauche(positionVerification);
+		verificationBas(positionVerification);
+
 	// Coin bas gauche
 	} else if ( $('#'+position).hasClass('col1') & $('#'+position).is('#row'+plateau.nbLignes) ) {
-		for (var m=1;m<4;m++){
-			if ($('#'+(position+m)).hasClass('empty') || $('#'+(position+m)).hasClass('weapon')) {
-				$('#'+(position+m)).addClass('movementPossible');
-			} else {break;}
-		}	
-		for (var m=1;m<4;m++){
-			if ($('#'+(position-plateau.nbColones*m)).hasClass('empty') || $('#'+(position-plateau.nbColones*m)).hasClass('weapon')) {
-				$('#'+(position-plateau.nbColones*m)).addClass('movementPossible');
-			} else {break;}	
-		}
+		verificationDroite(positionVerification);
+		verificationHaut(positionVerification);
 
 	// Coin bas droite
 	} else if ( $('#'+position).hasClass('col'+plateau.nbColones) & $('#'+position).is('#row'+plateau.nbLignes) ) {
-		for (var m=1;m<4;m++){
-			if ($('#'+(position-m)).hasClass('empty') || $('#'+(position-m)).hasClass('weapon')) {
-				$('#'+(position-m)).addClass('movementPossible');
-			} else {break;}
-		}
-
-		for (var m=1;m<4;m++){
-			if ($('#'+(position-plateau.nbColones*m)).hasClass('empty') || $('#'+(position-plateau.nbColones*m)).hasClass('weapon')) {
-				$('#'+(position-plateau.nbColones*m)).addClass('movementPossible');
-			} else {break;}	
-		}
+		veriificationGauche(positionVerification);
+		verificationHaut(positionVerification);
 
 	// Colone de gauche
 	} else if ( $('#'+position).hasClass('col1') ){
 
-		for (var m=1;m<4;m++){
-			if ($('#'+(position+m)).hasClass('empty') || $('#'+(position+m)).hasClass('weapon')) {
-				$('#'+(position+m)).addClass('movementPossible');
-			} else {break;}
-		}
-		for (var m=1;m<4;m++){
-			if ($('#'+(position-plateau.nbColones*m)).hasClass('empty') || $('#'+(position-plateau.nbColones*m)).hasClass('weapon')) {
-				$('#'+(position-plateau.nbColones*m)).addClass('movementPossible');
-			} else {break;}	
-		}
-		for (var m=1;m<4;m++){
-			if ($('#'+(position+plateau.nbColones*m)).hasClass('empty') || $('#'+(position+plateau.nbColones*m)).hasClass('weapon')) {
-				$('#'+(position+plateau.nbColones*m)).addClass('movementPossible');
-			} else {break;}		
-		}
+		verificationDroite(positionVerification);
+		verificationBas(positionVerification);
+		verificationHaut(positionVerification);
 
 	// Colone de droite
 	} else if ( $('#'+position).hasClass('col'+plateau.nbColones) ) {
-		for (var m=1;m<4;m++){
-			if ($('#'+(position-m)).hasClass('empty') || $('#'+(position-m)).hasClass('weapon')) {
-				$('#'+(position-m)).addClass('movementPossible');
-			} else {break;}
-		}
-		for (var m=1;m<4;m++){
-			if ($('#'+(position-plateau.nbColones*m)).hasClass('empty') || $('#'+(position-plateau.nbColones*m)).hasClass('weapon')) {
-				$('#'+(position-plateau.nbColones*m)).addClass('movementPossible');
-			} else {break;}	
-		}
-		for (var m=1;m<4;m++){
-			if ($('#'+(position+plateau.nbColones*m)).hasClass('empty') || $('#'+(position+plateau.nbColones*m)).hasClass('weapon')) {
-				$('#'+(position+plateau.nbColones*m)).addClass('movementPossible');
-			} else {break;}		
-		}
+		verificationGauche(positionVerification);
+		verificationBas(positionVerification);
+		verificationHaut(positionVerification);
 
 	// Ligne du haut
 	} else if ( $('#'+position).is('row1') ) {
-		for (var m=1;m<4;m++){
-			if (((position+m)%plateau.nbLignes===0) & ($('#'+(position+m)).hasClass('empty') || $('#'+(position+m)).hasClass('weapon'))) {
-				$('#'+(position+m)).addClass('movementPossible');
-				break;
-			} else if ($('#'+(position+m)).hasClass('empty') || $('#'+(position+m)).hasClass('weapon')) {
-				$('#'+(position+m)).addClass('movementPossible');
-			} else {break;}
-		}
-		for (var m=1;m<4;m++){
-			if ($('#'+(position-m)).hasClass('empty') || $('#'+(position-m)).hasClass('weapon')) {
-				$('#'+(position-m)).addClass('movementPossible');
-			} else {break;}
-		}
-		for (var m=1;m<4;m++){
-			if ($('#'+(position+plateau.nbColones*m)).hasClass('empty') || $('#'+(position+plateau.nbColones*m)).hasClass('weapon')) {
-				$('#'+(position+plateau.nbColones*m)).addClass('movementPossible');
-			} else {break;}		
-		}
+		verificationDroiteModulo(positionVerification);
+		verificationGauche(positionVerification);
+		verificationBas(positionVerification);
 
 	// Ligne du bas			
 	} else if ( $('#'+position).is('row'+plateau.nbLignes) ) {
-		for (var m=1;m<4;m++){
-			if ( position= ($('#'+(position+m)).hasClass('empty') || $('#'+(position+m)).hasClass('weapon'))){
-
-			} else if ($('#'+(position+m)).hasClass('empty') || $('#'+(position+m)).hasClass('weapon')) {
-				$('#'+(position+m)).addClass('movementPossible');
-			} else {break;}
-		}
-		for (var m=1;m<4;m++){
-			if (((position-m)%plateau.nbLignes===1) & ($('#'+(position-m)).hasClass('empty') || $('#'+(position-m)).hasClass('weapon'))) {
-				$('#'+(position-m)).addClass('movementPossible');
-				break;
-			}else if ($('#'+(position-m)).hasClass('empty') || $('#'+(position-m)).hasClass('weapon')) {
-				$('#'+(position-m)).addClass('movementPossible');
-			} else {break;}
-		}
-		for (var m=1;m<4;m++){
-			if ($('#'+(position-plateau.nbColones*m)).hasClass('empty') || $('#'+(position-plateau.nbColones*m)).hasClass('weapon')) {
-				$('#'+(position-plateau.nbColones*m)).addClass('movementPossible');
-			} else {break;}	
-		}	
+		verificationDroite(positionVerification);
+		verificationGaucheModulo(positionVerification);
+		verificationHaut(positionVerification);
 
 	} else {
-		for (var m=1;m<4;m++){
-			if (((position+m)%plateau.nbLignes===0) & ($('#'+(position+m)).hasClass('empty') || $('#'+(position+m)).hasClass('weapon'))) {
-				$('#'+(position+m)).addClass('movementPossible');
-				break;
-			} else if ($('#'+(position+m)).hasClass('empty') || $('#'+(position+m)).hasClass('weapon')) {
-				$('#'+(position+m)).addClass('movementPossible');
-			} else {break;}
-		}
-		for (var m=1;m<4;m++){
-			if (((position-m)%plateau.nbLignes===1) & ($('#'+(position-m)).hasClass('empty') || $('#'+(position-m)).hasClass('weapon'))) {
-				$('#'+(position-m)).addClass('movementPossible');
-				break;
-			}else if ($('#'+(position-m)).hasClass('empty') || $('#'+(position-m)).hasClass('weapon')) {
-				$('#'+(position-m)).addClass('movementPossible');
-			} else {break;}
-		}
-		for (var m=1;m<4;m++){
-			if ($('#'+(position+plateau.nbColones*m)).hasClass('empty') || $('#'+(position+plateau.nbColones*m)).hasClass('weapon')) {
-				$('#'+(position+plateau.nbColones*m)).addClass('movementPossible');
-			} else {break;}	
-		}
-		for (var m=1;m<4;m++){				
-			if ($('#'+(position-plateau.nbColones*m)).hasClass('empty') || $('#'+(position-plateau.nbColones*m)).hasClass('weapon')) {
-				$('#'+(position-plateau.nbColones*m)).addClass('movementPossible');
-			} else {break;}		
-		}
+		verificationGaucheModulo(positionVerification);
+		verificationDroiteModulo(positionVerification);
+		verificationBas(positionVerification);
+		verificationHaut(positionVerification);
 	}
 }
 
